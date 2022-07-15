e56663f3703ffb484f637c8655c00e901bffd312
Precious Kalu <56025991+Preciouskalu@users.noreply.github.com>
2022-07-15 19:30:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+// alert('the assignment works')
+
+// Calculate and return the sum of the numbers in an array.
+
+
+
+// function sumOfNumbers() {
+// let sum = 0;
+// const arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+    
+// for( let i = 0; i < arrayOfNumbers.length; i++ ){
+//     sum += arrayOfNumbers[i];
+//    console.log(sum)
+
+// }
+// }
+
+// sumOfNumbers()
+
+// // console.log(`The number of elements in the array is ${arrayOfNumbers.length}`)
+
+
+function sumOfNumbers() {
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+console.log(sum);
+}
+
+sumOfNumbers()
+
+
+
+// Count all even numbers within an array of numbers.
+
+// let arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+
+// function countEvenNumbers(arrayOfNumbers) {
+
+// // console.log(`The original array is [${arrayOfNumbers}] and it consists of ${arrayOfNumbers.length} elements`)
+// console.log('The original array of numbers is');       
+// console.log(arrayOfNumbers)
+
+// console.log('')
+// console.log('')
+
+
+// let evenNumbers = arrayOfNumbers.filter(number => number % 2 == 0)
+// console.log('The array of even numbers is');
+// console.log(evenNumbers)
+
+// console.log(`and there are ${evenNumbers.length} even numbers in this array`)
+
+
+//     return arrayOfNumbers.length;
+// }
+
+// countEvenNumbers(arrayOfNumbers)
+
+
+
+
+
+
+
+
+
+ 
+
+
